abc3171bc5aff60da39528eb107b398faecb0b9c
BrycenBissell <63610837+BrycenBissell@users.noreply.github.com>
2021-08-17 15:51:58 -0700

    Add files via upload

---
+++ moreconnectedthanyouthinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  moreconnectedthanyouthinkApp.swift
+//  moreconnectedthanyouthink
+//
+//  Created by Brycen Bissell on 5/27/21.
+//
+
+import SwiftUI
+
+@main
+struct moreconnectedthanyouthinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Start_screen()
+        }
+    }
+}
